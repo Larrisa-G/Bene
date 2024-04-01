456850a5a20af29dd058fc5ffd4f6958fe096886
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-04-01 15:23:52 -0300

    fazendo pessoa jurídica

--- src/main/java/com/app/entidades/PessoaFisica.java
+++ src/main/java/com/app/entidades/PessoaFisica.java
@@ -16,24 +16,13 @@ class PessoaFisica extends Pessoa {
     private String dataNascimento;
 
    public PessoaFisica( 
-        String nome,
-        String cpf,
-        String logradouro,
-        int numero,
-        String complemento,
-        String bairro,
-        String cep,
-        String cidade,
-        String uf,
-        String genero,
-        String rg,
-        String dataNascimento
-   ) {
+        String nome, String cpf, String logradouro,
+        int numero, String complemento, String bairro,
+        String cep, String cidade, String uf,
+        String genero, String rg, String dataNascimento) {
        super(nome, cpf, logradouro, numero, complemento, bairro, cep, cidade, uf);
        this.rg = rg;
        this.genero = genero;
        this.dataNascimento = dataNascimento;
-   }
-
-   
+   }  
 }
---
+++ src/main/java/com/app/entidades/PessoaJuridica.java
@@ -0,0 +1,26 @@
+package com.app.entidades;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class PessoaJuridica extends Pessoa{
+    
+    private String cnpj;
+    private String inscricaoEstadual;
+    private String fundacao;
+    
+    public PessoaJuridica(
+            String nome, String cpf, String logradouro, int numero, 
+            String complemento, String bairro, String cep, String cidade,
+            String uf,String cnpj,String inscricaoEstadual,String fundacao) {
+        super(nome, cpf, logradouro, numero, complemento, bairro, cep, cidade, uf);
+        this.cnpj = cnpj;
+        this.inscricaoEstadual = inscricaoEstadual;
+        this.fundacao = fundacao;
+    }
+    
+
+
+}
