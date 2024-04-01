d37b497dfea49cd9e29b3dd3363072c415ec052c
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-04-01 14:22:26 -0300

    Template para base do projeto

---
+++ src/main/java/com/app/TrabJava.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.app;
+
+/**
+ *
+ * @author 202108083852
+ */
+public class TrabJava {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
