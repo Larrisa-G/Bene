0cd765989bfdafcf453ac2ac1159a2ede8a7ae3e
Gabriel Conceição <gabrielc2019@outlook.com.br>
2024-06-02 17:20:37 -0300

    Mostrando dados Pessoa Fisica

--- src/main/java/com/app/paginas/pessoa/FormularioListarClientes.java
+++ src/main/java/com/app/paginas/pessoa/FormularioListarClientes.java
@@ -21,6 +21,7 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
         initComponents();
         jrbFisica.setSelected(true);
         criarTabela();
+        jpDadosPessoais.setVisible(false);
     }
     
     private void criarTabela(){
@@ -28,7 +29,7 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
                
                 new Object[] {"centro","esquerda"}, 
                 new Object[] {"CPF","Nome"},
-                jtCPF
+                jtSearch
         ); 
         
         model = (DefaultTableModel) table.getModel();       
@@ -52,11 +53,24 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
         jPanel1 = new javax.swing.JPanel();
         jbFechar = new javax.swing.JButton();
         jbListar = new javax.swing.JButton();
-        jtCPF = new javax.swing.JTextField();
+        jtSearch = new javax.swing.JTextField();
         searchLabel = new javax.swing.JLabel();
         jrbFisica = new javax.swing.JRadioButton("Física");
         jrbJuridica = new javax.swing.JRadioButton("Jurídica");
         jpnConsulta = new javax.swing.JPanel();
+        jpDadosPessoais = new javax.swing.JPanel();
+        jLabel1 = new javax.swing.JLabel();
+        jLabel2 = new javax.swing.JLabel();
+        jLabel3 = new javax.swing.JLabel();
+        jLabel4 = new javax.swing.JLabel();
+        jLabel5 = new javax.swing.JLabel();
+        jLabel6 = new javax.swing.JLabel();
+        jlRg = new javax.swing.JLabel();
+        jlGenero = new javax.swing.JLabel();
+        jlEstadoCivil = new javax.swing.JLabel();
+        jlProfissao = new javax.swing.JLabel();
+        jlDataNascimento = new javax.swing.JLabel();
+        jlNacionalidade = new javax.swing.JLabel();
 
         buttonGroup.add(jrbFisica);
         buttonGroup.add(jrbJuridica);
@@ -99,7 +113,7 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
                 .addGap(15, 15, 15)
                 .addComponent(searchLabel)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                .addComponent(jtCPF, javax.swing.GroupLayout.DEFAULT_SIZE, 326, Short.MAX_VALUE)
+                .addComponent(jtSearch, javax.swing.GroupLayout.DEFAULT_SIZE, 536, Short.MAX_VALUE)
                 .addGap(14, 14, 14)
                 .addComponent(jrbFisica)
                 .addGap(18, 18, 18)
@@ -117,7 +131,7 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
                     .addComponent(jbFechar)
                     .addComponent(jbListar)
-                    .addComponent(jtCPF, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jtSearch, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                     .addComponent(searchLabel)
                     .addComponent(jrbFisica)
                     .addComponent(jrbJuridica))
@@ -130,11 +144,93 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
         jpnConsulta.setLayout(jpnConsultaLayout);
         jpnConsultaLayout.setHorizontalGroup(
             jpnConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 675, Short.MAX_VALUE)
+            .addGap(0, 0, Short.MAX_VALUE)
         );
         jpnConsultaLayout.setVerticalGroup(
             jpnConsultaLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 200, Short.MAX_VALUE)
+            .addGap(0, 156, Short.MAX_VALUE)
+        );
+
+        jpDadosPessoais.setBorder(javax.swing.BorderFactory.createEtchedBorder(new java.awt.Color(102, 102, 102), null));
+
+        jLabel1.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel1.setText("Genero");
+
+        jLabel2.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel2.setText("RG");
+
+        jLabel3.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel3.setText("Estado Civil");
+
+        jLabel4.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel4.setText("Nacionalidade");
+
+        jLabel5.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel5.setText("Profissão");
+
+        jLabel6.setFont(new java.awt.Font("Courier New", 1, 14)); // NOI18N
+        jLabel6.setText("Data de Nascimento");
+
+        javax.swing.GroupLayout jpDadosPessoaisLayout = new javax.swing.GroupLayout(jpDadosPessoais);
+        jpDadosPessoais.setLayout(jpDadosPessoaisLayout);
+        jpDadosPessoaisLayout.setHorizontalGroup(
+            jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpDadosPessoaisLayout.createSequentialGroup()
+                .addGap(15, 15, 15)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                    .addGroup(jpDadosPessoaisLayout.createSequentialGroup()
+                        .addComponent(jLabel5)
+                        .addGap(116, 116, 116)
+                        .addComponent(jLabel3))
+                    .addComponent(jLabel4)
+                    .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jpDadosPessoaisLayout.createSequentialGroup()
+                        .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlRg, javax.swing.GroupLayout.PREFERRED_SIZE, 170, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel2))
+                        .addGap(18, 18, 18)
+                        .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jlGenero, javax.swing.GroupLayout.PREFERRED_SIZE, 169, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(jLabel1)))
+                    .addGroup(jpDadosPessoaisLayout.createSequentialGroup()
+                        .addComponent(jlProfissao, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                            .addComponent(jLabel6)
+                            .addComponent(jlEstadoCivil, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                    .addGroup(jpDadosPessoaisLayout.createSequentialGroup()
+                        .addComponent(jlNacionalidade, javax.swing.GroupLayout.PREFERRED_SIZE, 177, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                        .addComponent(jlDataNascimento, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)))
+                .addContainerGap(270, Short.MAX_VALUE))
+        );
+        jpDadosPessoaisLayout.setVerticalGroup(
+            jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGroup(jpDadosPessoaisLayout.createSequentialGroup()
+                .addGap(22, 22, 22)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jLabel1, javax.swing.GroupLayout.PREFERRED_SIZE, 18, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel2))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jlGenero, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlRg, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel5)
+                    .addComponent(jLabel3))
+                .addGap(3, 3, 3)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(jlProfissao, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlEstadoCivil, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(jLabel4, javax.swing.GroupLayout.PREFERRED_SIZE, 20, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jLabel6))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jpDadosPessoaisLayout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                    .addComponent(jlNacionalidade, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(jlDataNascimento, javax.swing.GroupLayout.PREFERRED_SIZE, 17, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addContainerGap(68, Short.MAX_VALUE))
         );
 
         javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
@@ -144,8 +240,12 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
             .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
             .addGroup(layout.createSequentialGroup()
                 .addGap(15, 15, 15)
-                .addComponent(jpnConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addGroup(layout.createSequentialGroup()
+                        .addComponent(jpDadosPessoais, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                        .addGap(0, 250, Short.MAX_VALUE))
+                    .addComponent(jpnConsulta, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -153,7 +253,9 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
                 .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
                 .addGap(18, 18, 18)
                 .addComponent(jpnConsulta, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(249, Short.MAX_VALUE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                .addComponent(jpDadosPessoais, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addContainerGap())
         );
 
         pack();
@@ -165,7 +267,7 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
 
     private void jrbFisicaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jrbFisicaActionPerformed
         searchLabel.setText("CPF");
-        jtCPF.setText("");
+        jtSearch.setText("");
         model.setNumRows(0);
         model.setColumnIdentifiers(new Object[] {"CPF", "Nome"});
         try {
@@ -181,14 +283,28 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
     }//GEN-LAST:event_jrbFisicaActionPerformed
 
     private void jbListarActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jbListarActionPerformed
-        
+        if (searchLabel.getText() == "CPF") {
+            FisicaController fc = new FisicaController();
+            try {
+                Fisica f = fc.buscarUm(jtSearch.getText());
+                jlRg.setText(f.getRg());
+                jlGenero.setText(f.getGenero().name());
+                jlEstadoCivil.setText(f.getEstadoCivil().name());
+                jlNacionalidade.setText(f.getNacionalidade());
+                jlProfissao.setText(f.getProfissao());
+                jlDataNascimento.setText(f.getDataNascimento());
+                jpDadosPessoais.setVisible(true);
+            } catch (ServiceException e) {
+                JOptionPane.showMessageDialog(null, e.getMessage());
+            }
+        }
     }//GEN-LAST:event_jbListarActionPerformed
 
     private void jrbJuridicaActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_jrbJuridicaActionPerformed
         searchLabel.setText("CNPJ");
-        jtCPF.setText("");
+        jtSearch.setText("");
         model.setNumRows(0);
-        model.setColumnIdentifiers(new Object[] {"CNPJ", "Empresa","Diretor"});
+        model.setColumnIdentifiers(new Object[] {"CNPJ", "Empresa","CPF Diretor","Diretor"});
         try {
             JuridicaController jc = new JuridicaController();
             List<Juridica> list = jc.buscarTodos();
@@ -196,7 +312,8 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
             
             for (Juridica j : list) {
                 FisicaController fc = new FisicaController();
-                model.addRow(new Object[] {j.getCnpj(),j.getNomeFantasia(), fc.buscarUm(j.getCpfDiretor()).getNome()});
+                
+                model.addRow(new Object[] {j.getCnpj(),j.getNomeFantasia(),j.getCpfDiretor(),fc.buscarUm(j.getCpfDiretor()).getNome()});
             }
         } catch (ServiceException e) {
             JOptionPane.showMessageDialog(null, e.getMessage());
@@ -207,13 +324,26 @@ public class FormularioListarClientes extends javax.swing.JInternalFrame {
 
     // Variables declaration - do not modify//GEN-BEGIN:variables
     private javax.swing.ButtonGroup buttonGroup;
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JLabel jLabel5;
+    private javax.swing.JLabel jLabel6;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JButton jbFechar;
     private javax.swing.JButton jbListar;
+    private javax.swing.JLabel jlDataNascimento;
+    private javax.swing.JLabel jlEstadoCivil;
+    private javax.swing.JLabel jlGenero;
+    private javax.swing.JLabel jlNacionalidade;
+    private javax.swing.JLabel jlProfissao;
+    private javax.swing.JLabel jlRg;
+    private javax.swing.JPanel jpDadosPessoais;
     private javax.swing.JPanel jpnConsulta;
     private javax.swing.JRadioButton jrbFisica;
     private javax.swing.JRadioButton jrbJuridica;
-    private javax.swing.JTextField jtCPF;
+    private javax.swing.JTextField jtSearch;
     private javax.swing.JLabel searchLabel;
     // End of variables declaration//GEN-END:variables
 }
